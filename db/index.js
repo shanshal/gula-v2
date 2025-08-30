f0c6b48d9bf6062a0e33c7b949c0c4e1c0a80d05
shanshal <ahmedmahdii2003@gmail.com>
2025-08-30 16:34:40 +0300

    Update db config

--- db/index.js
+++ db/index.js
@@ -1,4 +1,15 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "dpg-d2gdte0dl3ps73f7oku0-a",
+  port: 5432,
+  database: "guladb_louu",
+  user: "guladb_louu_user",
+  password: "bnkKizx206Ec0A7g4yE2JUtKi15Tce5u",
+  ssl: { rejectUnauthorized: false }, // required for Render DB
+});
+
+/*
 const pool = new Pool({
   user: 'shanshal',
   host: 'localhost',
@@ -7,3 +18,4 @@ const pool = new Pool({
   port: 5432,
 });
 module.exports = pool;
+*/
