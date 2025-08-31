a1d907f8d04dd22971adda75ab43dcc801837727
shanshal <ahmedmahdii2003@gmail.com>
2025-08-31 09:26:09 +0300

    Removed question routes from swagger

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,52 +2,6 @@ const express = require('express');
 const router = express.Router();
 const questionController = require('../controllers/questionController');
 
-/**
- * @swagger
- * /questions:
- *   get:
- *     summary: Get all questions
- *     tags: [Questions]
- *     responses:
- *       200:
- *         description: List of questions
- */
-
-/**
- * @swagger
- * /questions/{id}:
- *   get:
- *     summary: Get question by ID
- *     tags: [Questions]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: Question object
- *       404:
- *         description: Question not found
- */
-
-/**
- * @swagger
- * /questions/survey/{surveyId}:
- *   get:
- *     summary: Get all questions for a specific survey
- *     tags: [Questions]
- *     parameters:
- *       - in: path
- *         name: surveyId
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: List of questions for the survey
- */
 router.get('/survey/:surveyId', questionController.getQuestionsBySurveyId);
 
 router.get('/', questionController.getQuestions);
