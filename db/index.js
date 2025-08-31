c149dada475e6720d27d7c0af449276d1060e89b
shanshal <ahmedmahdii2003@gmail.com>
2025-08-31 21:18:19 +0300

    Change db back to prod

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: 'shanshal',
-  host: 'localhost',
-  database: 'gula_v2',
-  password: 'RUSTneversl33ps',
+ const pool = new Pool({
+  host: "dpg-d2gdte0dl3ps73f7oku0-a",
   port: 5432,
+  database: "guladb_louu",
+  user: "guladb_louu_user",
+  password: "bnkKizx206Ec0A7g4yE2JUtKi15Tce5u",
+  ssl: { rejectUnauthorized: false }, // required for Render DB
 });
 
 /*
